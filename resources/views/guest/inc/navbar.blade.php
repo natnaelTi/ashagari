56a699f62f42c761031ab64f08ec0d1d33cd15a3
Natnael Tilaye <natnaeltilaye30@gmail.com>
2022-05-19 20:55:44 +0300

    refactored event link on navbar

--- resources/views/guest/inc/navbar.blade.php
+++ resources/views/guest/inc/navbar.blade.php
@@ -28,7 +28,7 @@
                             <a class="nav-link" href="about.html">About</a>
                          </li>
                          <li class="nav-item ">
-                            <a class="nav-link" href="Upcoming.html"> Events</a>
+                            <a class="nav-link" href="Upcoming.html">Conferences</a>
                          </li>
 
                       </ul>
